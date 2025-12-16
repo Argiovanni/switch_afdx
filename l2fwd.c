a61319aaeae3fbc1cbbd66b17080ba1845c55b4b
Alexandre Vanicotte <alexandre.vanicottehochman@etu.inp-n7.fr>
2025-12-16 14:55:54 +0100

    timing mais en coherent

--- l2fwd.c
+++ l2fwd.c
@@ -122,6 +122,7 @@ struct l2fwd_port_statistics
 } __rte_cache_aligned;
 struct l2fwd_port_statistics port_statistics[RTE_MAX_ETHPORTS];
 
+static time_t[rte_eth_dev_count()] port_start;
 #define MAX_TIMER_PERIOD 86400 /* 1 day max */
 /* A tsc-based timer responsible for triggering statistics printout */
 static uint64_t timer_period = 10; /* default period is 10 seconds */
@@ -357,8 +358,6 @@ l2fwd_main_loop(void)
 		RTE_LOG(INFO, L2FWD, " -- lcoreid=%u portid=%u\n", lcore_id,
 				portid);
 	}
-	time_t start;
-	time_t end;
 	while (!force_quit)
 	{
 
@@ -378,8 +377,7 @@ l2fwd_main_loop(void)
 				sent = rte_eth_tx_buffer_flush(portid, 0, buffer);
 
 				if (sent)
-					end  = time.now();
-					port_statistics[max_time]= (port_statistics[max_time]>(end-start))?port_statistics[max_time]:end-start;
+					port_statistics[max_time]= (port_statistics[max_time]>(time.now()-port_start[port_id]))?port_statistics[max_time]:time.now()-port_start[port_id];
 					port_statistics[portid].tx += sent;
 			}
 
@@ -416,7 +414,7 @@ l2fwd_main_loop(void)
 			portid = qconf->rx_port_list[i];
 			nb_rx = rte_eth_rx_burst((uint8_t)portid, 0,
 									 pkts_burst, MAX_PKT_BURST);
-			start = time.now();
+			port_start[port_id] = time.now();
 			port_statistics[portid].rx += nb_rx;
 			for (j = 0; j < nb_rx; j++)
 			{
@@ -760,7 +758,7 @@ int main(int argc, char **argv)
 
 	nb_ports_available = nb_ports;
 
-	/* Initialise each port */
+	/* Initialise each port */	
 	for (portid = 0; portid < nb_ports; portid++)
 	{
 		/* skip ports that are not enabled */
